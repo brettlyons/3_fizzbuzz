7de184b31b84faf53ddce7e0a942acf949324ad5
Brett Lyons <blyons@students.naropa.edu>
2015-04-21 17:00:04 -0600

    changed filename to fizzbuzzfunc.py

---
+++ fizzbuzzfunc.py
@@ -0,0 +1,14 @@
+def fizzBuzz(intList):
+    newList = list(intList)
+        # newList is the list to be returned, copy to prevent unwanted mutation
+        # of intList.
+        for i in range(len(intList)):
+            if intList[i] % 5 == 0 and intList[i] % 3 == 0:
+                newList[i] = "FizzBuzz"
+                elif intList[i] % 3 == 0:
+                    newList[i] = "Fizz"
+                elif intList[i] % 5 == 0:
+                    newList[i] = "Buzz"
+                else:
+                    newList[i] = intList[i]
+        return newList
